1da7273121b7af18f1c8997dad86d03b7c9246ef
Aurora ❄ <alvs131313@gmail.com>
2024-11-14 10:38:07 +0800

    INITIAL COMMIT

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import logo from './assets/logo.svg'
+
+function App() {
+
+  return (
+    <div className="w-full h-screen bg-black text-white flex items-center justify-center">
+      <img src={logo} alt="logo" />
+    </div>
+  )
+}
+
+export default App
