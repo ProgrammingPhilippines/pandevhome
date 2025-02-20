6d7ed726df2bc2a7d1160fcb4eea2ad9819fe8ef
Jericho Serrano <118679068+jericho1050@users.noreply.github.com>
2025-02-21 01:19:50 +0800

    chore: migrated to tailwind v4.0.7

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,23 +7,7 @@ export default {
 		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
-		container: {
-			center: true,
-			padding: "2rem",
-			screens: {
-				"2xl": "1400px",
-			},
-		},
 		extend: {
-			// see /App.css for reference
-			colors: {
-				background: "hsl(var(--background))",
-				foreground: "hsl(var(--foreground))",
-				primary: "hsl(var(--primary))",
-				secondary: "hsl(var(--secondary))",
-				accent: "hsl(var(--accent))",
-				'primary-button': "hsl(var(--primary-button-color))",
-			},
 		},
 	},
 	plugins: [],
