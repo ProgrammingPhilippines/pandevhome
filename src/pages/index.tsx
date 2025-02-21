8db3c4f0139fa88e26c55ca626af820f318ffcaf
Jericho Serrano <118679068+jericho1050@users.noreply.github.com>
2025-02-21 19:15:33 +0800

    chore: deifning html structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,69 @@
 import logo from "@/assets/logo.png";
-import {cn} from "@/util/cn";
-import {buttonVariants} from "@/components/ui/button";
-
+import menu from "@/assets/hamburger-menu.svg";
+import exit from "@/assets/exit.svg";
+import Image from "next/image";
+import discord1 from "@/assets/discord-icon-purp.svg";
+import discord2 from "@/assets/discord-icon-white.svg";
+import github from "@/assets/github-mark.svg";
+import browser from "@/assets/browser-icon.svg";
+import x from "@/assets/x-icon.svg";
 export default function Home() {
 	return (
-		<div className="min-h-screen bg-background text-foreground">
-			<h1>
-				<div className="container min-h-screen flex items-center justify-center">
-					<div className="flex flex-col space-y-4">
-						<img src={logo.src} alt="logo"/>
-						<a
-							className={cn(buttonVariants({className: "text-lg rounded-full bg-primary-btn", size: "lg"}))}
-							href={process.env.NEXT_PUBLIC_DISCORD_URL}
-							rel="noreferrer"
-							target="_blank"
-						>
-							Join Discord
+		<div className="min-h-screen">
+			<header>
+				<Image src={logo} alt="The pandev logo" id="logo" />
+				<nav>
+					<Image src={menu} alt="Hamburger menu icon" id="menu-btn" />
+					<ul className="hidden">
+						<li>
+							<Image src={exit} alt="Exit menu icon" />
+						</li>
+						<li>Home</li>
+						<li>About Ul</li>
+						<li>Team</li>
+						<li>Testimonials</li>
+					</ul>
+					<div className="hidden">
+						<p>get in touch</p>
+						<a href="devteam@gmail.com" type="email">
+							devteam@gmail.com
 						</a>
 					</div>
+					<div className="hidden">
+						<p>follow us on social medial</p>
+						<div>
+							<Image src={discord1} alt="Discord icon" />
+							<Image src={github} alt="Github icon" />
+							<Image src={browser} alt="Browser icon" />
+							<Image src={x} alt="X icon" />
+						</div>
+					</div>
+				</nav>
+			</header>
+			<main>
+				<h1>
+					a comm<span>unity</span> of <span>filipino</span> software developers
+				</h1>
+				<p>Pandev is where the fun tech talks take place</p>
+				<a href={process.env.NEXT_PUBLIC_DISCORD_URL}>
+					<Image src={discord2} alt="Discord Icon White" />
+					Join Discord
+				</a>
+				<div>
+					<div>
+						<p>1k+</p>
+						<p>Active Members</p>
+					</div>
+					<div>
+						<p>12k+</p>
+						<p>Meetups & Events</p>
+					</div>
+					<div>
+						<p>10k+</p>
+						<p>Raffles & Giveaways</p>
+					</div>
 				</div>
-			</h1>
+			</main>
 		</div>
 	);
 }
