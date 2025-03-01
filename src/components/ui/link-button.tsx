ecc79e0343fd7d59f4eae45d20e6f00e21f76571
almostJohn <garcia.johngale@gmail.com>
2025-03-01 09:59:10 +0800

    refactor: improve LinkButtonProps type definition

--- src/components/ui/link-button.tsx
+++ src/components/ui/link-button.tsx
@@ -5,11 +5,13 @@ import type { VariantProps } from "class-variance-authority";
 import { buttonVariants } from "./button";
 import { cn } from "@/util/cn";
 
-type LinkButtonProps = Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, keyof LinkProps> &
+type LinkButtonBaseComponent = HTMLElementTagNameMap["a"];
+
+type LinkButtonProps = Omit<React.AnchorHTMLAttributes<LinkButtonBaseComponent>, keyof LinkProps> &
 	LinkProps &
 	VariantProps<typeof buttonVariants>;
 
-export const LinkButton = React.forwardRef<HTMLAnchorElement, LinkButtonProps>(
+export const LinkButton = React.forwardRef<LinkButtonBaseComponent, LinkButtonProps>(
 	({ className, variant, size, href, ...props }, ref) => {
 		return <Link href={href} className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
 	},
